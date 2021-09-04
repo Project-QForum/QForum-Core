88119bb99badfad3ef777a0521a8da09ce49cb4d
JackuXL <jackuxl2019@gmail.com>
2021-09-04 20:10:01 +0800

    修复Kotlin不完全兼容的问题

--- src/main/java/cn/jackuxl/qforum/QForumApplication.java
+++ src/main/java/cn/jackuxl/qforum/QForumApplication.java
@@ -3,11 +3,9 @@ package cn.jackuxl.qforum;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@MapperScan("cn.jackuxl.qforum")
+@MapperScan("cn.jackuxl.qforum.mapper")
 public class QForumApplication {
     public static void main(String[] args) {
         SpringApplication.run(QForumApplication.class, args);
--- src/main/java/cn/jackuxl/qforum/controller/BoardController.java
+++ src/main/java/cn/jackuxl/qforum/controller/BoardController.java
@@ -2,30 +2,26 @@ package cn.jackuxl.qforum.controller;
 
 import cn.jackuxl.qforum.model.Board;
 import cn.jackuxl.qforum.model.User;
-import cn.jackuxl.qforum.service.UserService;
 import cn.jackuxl.qforum.serviceimpl.BoardServiceImpl;
 import cn.jackuxl.qforum.serviceimpl.UserServiceImpl;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import java.util.Objects;
+
 @CrossOrigin
 @RestController
 public class BoardController {
     @Autowired
-    UserServiceImpl userService;
+    private UserServiceImpl userService;
     @Autowired
-    HttpServletResponse response;
+    private HttpServletResponse response;
     @Autowired
-    BoardServiceImpl boardService;
+    private BoardServiceImpl boardService;
 
     @RequestMapping(value = "/admin/addBoard", produces = "application/json;charset=UTF-8")
     public String addBoard(String sessionId, Board board) {
