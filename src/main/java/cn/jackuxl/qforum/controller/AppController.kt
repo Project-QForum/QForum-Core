45dce0225926351d8e65a4a48c8c8b0d5f66ef3b
dsr <1227731421@qq.com>
2022-01-16 23:24:26 +0800

    update AppController.kt;

--- src/main/java/cn/jackuxl/qforum/controller/AppController.kt
+++ src/main/java/cn/jackuxl/qforum/controller/AppController.kt
@@ -78,7 +78,7 @@ class AppController {
     }
 
     @RequestMapping(value = ["/app/getAppDetail"], produces = ["application/json;charset=UTF-8"])
-    fun getThreadDetail(packageName: String): ResultEntity<App> {
+    fun getThreadDetail(packageName: String): ResultEntity<AppVo> {
 
         val app = appService.getAppByPackageName(packageName)
         BasicUtil.assertTool(app!=null,"no_such_app")
@@ -91,6 +91,6 @@ class AppController {
         appVo.publisher = publisher
         appVo.tag = tagService.getTagById(app.tagId)
 
-        return Result.ok("success",app)
+        return Result.ok("success",appVo)
     }
 }
